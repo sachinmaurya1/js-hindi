5b834fe68724d09514bd7c53792e9d9ad93f4475
sachinmaurya1 <109779070+sachinmaurya1@users.noreply.github.com>
2023-12-23 06:13:55 +0000

    02_datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+
+//alert (2+2) // we are using nodejs, not browser
+// number
+// bigint
+// boolean
+// null 
+// undefined
+// symbol
+
+
+
+// object
+
+console.log(typeof "histesh")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
