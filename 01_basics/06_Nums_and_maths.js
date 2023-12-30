47ae32dc1662cc16bbeb4bdf0e78a69b481a5862
sachinmaurya1 <109779070+sachinmaurya1@users.noreply.github.com>
2023-12-30 06:36:58 +0000

    date and time

---
+++ 01_basics/06_Nums_and_maths.js
@@ -0,0 +1,25 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(111)
+// console.log(balance)
+
+// console.log(balance.toString())
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(1))
+
+// const otherNumber = 123.343
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 100000000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+console.log(Math)
+console.log(Math.abs(3)) // convert into absolute value
+console.log(Math.round(4.56)) // thre is another way using floor and ceiling 
+
+console.log(Math.max(4,3,5,6))
+
