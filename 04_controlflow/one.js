78522a8e79b9e51bbd410938ed72c2a9a5a9edc1
sachinmaurya1 <109779070+sachinmaurya1@users.noreply.github.com>
2024-01-01 17:14:30 +0000

    control flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,16 @@
+// control flow
+
+
+// const isUserLoggedIn = true
+// if(isUserLoggedIn){
+//     console.log("agar if ke ander wali condition true hai to hi if condition me controlflow jayega ")
+// }
+
+const score = 200
+
+if ( score >100){
+    const power = "fly"
+    console.log(`User power: ${power}`)
+}
+
+
