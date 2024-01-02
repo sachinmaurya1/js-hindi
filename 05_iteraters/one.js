8474d24f5e5b3c9ed971be7784b7cbffe19234ba
sachinmaurya1 <109779070+sachinmaurya1@users.noreply.github.com>
2024-01-02 14:06:29 +0000

    for loop

---
+++ 05_iteraters/one.js
@@ -0,0 +1,46 @@
+// for
+
+for (let i = 0; i <10; i++) {
+    const element = i;
+    if(element ==5){
+        console.log("5 is the best number")
+    }
+    // console.log(element)
+    
+}
+
+// console.log(element) // this is not excessable
+for(let i =1; i<=10; i++){
+    // console.log(`outer loop value: ${i}`)
+   for(let j=1; j<=10; j++){
+    // console.log(`inner loop value ${j} and inner loop  ${i}`)
+    // console.log(i + '*' + j + '=' + i*j)
+   }
+}
+
+let myArray = ["falsh","batman", "superman"]
+// console.log(myArray.length)
+for(let index =0; index < myArray.length;index++){
+    
+    const element = myArray[index]
+    // console.log(element)
+}
+
+
+// for(let index =0; index <= 20;index++){
+//     if(index ==5){
+//         console.log("detected 5")
+//         break;
+//     }
+//     const element = index
+//     console.log(element)
+// }
+for(let index =0; index <= 20;index++){
+    if(index ==5){
+        console.log("detected 5")
+        continue; // ek bar maaf kardo 
+    }
+    const element = index
+    console.log(element)
+}
+
