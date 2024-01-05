67e7fbd1dc22ca8e72c397aaa1c17900b8e10d19
sachinmaurya1 <109779070+sachinmaurya1@users.noreply.github.com>
2024-01-05 15:30:07 +0000

    foreach loop

---
+++ 05_iteraters/five.js
@@ -0,0 +1,12 @@
+const coding = ["js","ruby",'java','cpp']
+// for each loop for arrays using callback function
+coding.forEach(function (item){
+// console.log(item)
+})
+
+// forEach loop using arrow function
+coding.forEach((item)=>{
+// console.log(item)
+})
+
+// printMe function
--- 05_iteraters/four.js
+++ 05_iteraters/four.js
@@ -9,6 +9,23 @@ const myObject ={
 
 for (const key in myObject) {
 console.log(`${key} shortcut is for ${myObject[key]}`)
-        
     
 }
+
+const programming = ["js","rb","py","java"]
+
+for (const key in programming){
+    console.log(programming[key]);
+}
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA', "United State Of America")
+
+for (const key in map){
+    console.log(key) // this will not print anything because maps are not itertable 
+}
+
+// objects ke mostly hum for in loops lga denge 
+
+// arrays ke uper hum for of loop lga denge 
