5f5c53157f1377210dea00c2b1fe604f22cf853a
sachinmaurya1 <109779070+sachinmaurya1@users.noreply.github.com>
2024-01-19 17:52:46 +0000

    question

---
+++ practise/two.js
@@ -0,0 +1,13 @@
+var variable = 10;
+ (()=>{
+    console.log(variable);
+
+    variable = 20;
+
+    console.log(variable);
+
+ })(); // iife function hai ye 
+
+ console.log(variable)
+
+ var variable = 30;
